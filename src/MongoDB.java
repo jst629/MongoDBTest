59b602e3a62ef7b1784aa0a4f4f8fe9e31633924
Songtao Jiang <stjiang888@hotmail.com>
2016-08-24 11:04:33 -0700

    Add query

--- src/MongoDB.java
+++ src/MongoDB.java
@@ -8,8 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static java.util.Arrays.asList;
 
 /**
  * Servlet implementation class MongoDB
@@ -43,9 +55,54 @@ public class MongoDB extends HttpServlet {
 	}
 	
 	protected void establishDatabase(){
-		MongoClient dbClient = new MongoClient( "localhost" , 27017);
+		MongoClient dbClient = new MongoClient("localhost",27017);
 		MongoDatabase db = dbClient.getDatabase("test");
+		MongoCollection<Document> table = db.getCollection("restaurants");
+
 	}
 	
+	protected void insertData(MongoCollection<Document> table){
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+		try {
+			table.insertOne(
+			        new Document("address",
+			                new Document()
+			                        .append("street", "2 Avenue")
+			                        .append("zipcode", "10075")
+			                        .append("building", "1480")
+			                        .append("coord", asList(-73.9557413, 40.7720266)))
+			                .append("borough", "Manhattan")
+			                .append("cuisine", "Italian")
+			                .append("grades", asList(
+			                        new Document()
+			                                .append("date", format.parse("2014-10-01T00:00:00Z"))
+			                                .append("grade", "A")
+			                                .append("score", 11),
+			                        new Document()
+			                                .append("date", format.parse("2014-01-16T00:00:00Z"))
+			                                .append("grade", "B")
+			                                .append("score", 17)))
+			                .append("name", "Vella")
+			                .append("restaurant_id", "41704620"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	protected void queryData(MongoCollection<Document> table){
+		FindIterable<Document> iterable = table.find();
+		
+	    if (iterable == null) {
+	        return;
+	    }
+	    
+	    for (Document current : iterable) {
+	    	String cuisine = current.getString("cuisine");
+	    	
+	    }
+	    
+	}
 
 }
